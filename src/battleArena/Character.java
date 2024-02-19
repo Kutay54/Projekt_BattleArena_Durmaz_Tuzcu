2fb3a76ba8c1dd79ab94aeeb516fa11ea605383b
Kutay54 <Kutay.Tuzcu@hak-reutte.ac.at>
2024-02-19 11:22:15 +0100

    creating the remaining classes

--- src/battleArena/Character.java
+++ src/battleArena/Character.java
@@ -6,4 +6,56 @@ public abstract class Character {
 	private double lifePoints;
 	private boolean specialAbilityDevice;
 	
+	
+	public Character(String name) {
+		this.name = name;
+		this.lifePoints = 100;
+		this.specialAbilityDevice = true;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getLifePoints() {
+		return lifePoints;
+	}
+
+
+	public void setLifePoints(double lifePoints) {
+		this.lifePoints = lifePoints;
+	}
+
+
+	public boolean isSpecialAbilityDevice() {
+		return specialAbilityDevice;
+	}
+
+
+	public void setSpecialAbilityDevice(boolean specialAbilityDevice) {
+		this.specialAbilityDevice = specialAbilityDevice;
+	}
+	
+	
+	/**
+	 * is called when a character has been attacked
+	 * is deprived of health points as a result. 
+	 * @param points
+	 */
+	public void getDamage (int points) {
+		
+	}
+	
+	
+	public void attack (Character enemy) {
+		
+	}
+	
 }
---
+++ src/battleArena/Dragon.java
@@ -0,0 +1,12 @@
+package battleArena;
+
+public class Dragon extends Character{
+
+	public Dragon(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
